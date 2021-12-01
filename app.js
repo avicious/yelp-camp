b2ac5737607648a5fc72f453a0e9bb20ef4f5073
avicious <shubham2951@gmail.com>
2021-12-01 13:54:25 +0530

    Added basic styles and layout with Bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
@@ -13,6 +14,7 @@ db.once('open', () => {
     console.log('Database Connected');
 });
 
+app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
