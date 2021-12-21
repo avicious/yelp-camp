3b9215906201bccc03cf56161b528931b1481b32
avicious <shubham2951@gmail.com>
2021-12-21 12:33:36 +0530

    added more authorisation

--- seeds/index.js
+++ seeds/index.js
@@ -16,10 +16,11 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < 10; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground ({
+            author: '61c089b334130306ca811494',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
